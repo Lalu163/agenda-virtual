95948df2d53b20e6d27b8d69a1ab3dda24326d59
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2024-08-20 20:56:10 +0200

    creado repositorio

---
+++ src/main/java/com/todotic/agendavirtual/repository/ContactRepository.java
@@ -0,0 +1,9 @@
+package com.todotic.agendavirtual.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.todotic.agendavirtual.entity.Contact;
+
+public interface ContactRepository extends CrudRepository<Contact, Integer>{
+
+}
