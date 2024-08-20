22622642a1040f19837259e9530c327365b02510
Laura Contreras <lauracontreras@MacBook-Pro-de-Laura.local>
2024-08-20 20:29:04 +0200

    first commit

---
+++ src/test/java/com/todotic/agendavirtual/AgendavirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todotic.agendavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendavirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
